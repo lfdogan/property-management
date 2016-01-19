ddf3c493927d8e58497556c78a6f7e11f3b30552
Lauren Dogan <laurenfdogan@gmail.com>
2016-01-19 14:35:46 -0500

    add styling

--- app/scripts/controllers/BillsCtrl.js
+++ app/scripts/controllers/BillsCtrl.js
@@ -1,11 +1,11 @@
  (function() {
-     function BillsCtrl(Bills) { //services injected
+     function BillsCtrl(Data) { //services injected
 
          
 //variables accessed by the html page as completed.<variable> where it is defined here as this.<variable>         
          this.title = "Bills";
-         this.allBills = Bills.billNumber(); // the ARRAY of objects from the allMessages Firebase database
-         this.Bills = Bills; // allows html to access functions in Message service
+         this.allBills = Data.billNumber(); // the ARRAY of objects from the Firebase database
+         //this.Data = Data; // allows html to access functions in Data service
 
          
 
@@ -18,7 +18,7 @@
  
      angular
          .module('propertyManagement')
-         .controller('BillsCtrl', ['Bills', BillsCtrl]); //array of dependencies contains services and lastly callback function. Inject $firebaseObject and other services into this controller
+         .controller('BillsCtrl', ['Data', BillsCtrl]); //array of dependencies contains services and lastly callback function. Inject $firebaseObject and other services into this controller
  })();
 
 /* 1/12/16 when moving functions out of BillsCtrl.js into Message.js you must have:
